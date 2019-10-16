546e59ffb68f56d96d811db56b22651247e01c18
muralimgwl <murali.m@goodworklabs.com>
2019-10-16 13:22:42 +0530

    test

--- src/api/index.js
+++ src/api/index.js
@@ -4,57 +4,90 @@ import { Router } from "express";
 export default ({ config, db }) => {
   let api = Router();
 
-  api.get("/company", (req, res) => {
-    //find id in company table and return the company
-    db.query("SELECT * from company", (err, response) => {
+  api.get("/task", (req, res) => {
+    //find id in task table and return the task
+    db.query("SELECT * from tasks", (err, response) => {
       if (err) {
         console.log(err.stack);
       } else {
         console.log(response.rows);
-				res.json({"companies":response.rows});
+        res.json({ tasks: response.rows });
       }
     });
-
-
   });
   // perhaps expose some API metadata at the root
-  api.get("/company/:id", (req, res) => {
-    //find id in company table and return the company
-    db.query(`SELECT * from company where id=${req.params.id}`, (err, response) => {
-      if (err) {
-        console.log(err.stack);
-      } else {
-        console.log(response.rows);
-				res.json({"companies":response.rows});
-      }
-    });
-  });
+  // api.get("/task/:id", (req, res) => {
+  //   //find id in task table and return the task
+  //   db.query(`SELECT * from task where id=${req.params.id}`, (err, response) => {
+  //     if (err) {
+  //       console.log(err.stack);
+  //     } else {
+  //       console.log(response.rows);
+  // 			res.json({"companies":response.rows});
+  //     }
+  //   });
+  // });
 
-  api.post("/company", (req, res) => {
-    //take company from req and insert into company table
+  api.post("/task", (req, res) => {
+    //take task from req and insert into task table
     console.log("body", req.body);
-    const {name,address,phonenumber}=req.body;
-    db.query(`insert into company values(${new Date().getTime()},'${name}','${address}',${phonenumber})`, (err, response) => {
-      if (err) {
-        console.log(err.stack);
-      } else {
-        console.log(response.rows);
-				res.json({"status":"successfull","response":response.rows});
+    const { name } = req.body;
+    const uuidv1 = require("uuid/v1");
+    const id = uuidv1();
+    const createdBy = "200";
+    const createTime = new Date().toJSON();
+    db.query(
+      `insert into tasks values('${id}','${name}',false,'${createdBy}','${createTime}')`,
+      (err, response) => {
+        if (err) {
+          console.log(err.stack);
+        } else {
+          console.log(response.rows);
+          res.json({ status: "successfull"});
+        }
       }
-    });
+    );
   });
 
-  api.put("/company/:id", (req, res) => {
+  api.put("/task/:id", (req, res) => {
     console.log("req", req.params);
     console.log("body", req.body);
-    //take company id from path and find the id and update
-    res.json({ version, status: "live", method: "put" });
+    console.log("body", req.body);
+    const { id } = req.params;
+    const { status } = req.body;
+    const updatedBy = "200";
+    const updateTime = new Date().toJSON();
+    db.query(
+      `UPDATE tasks
+	SET status=true,"updatedBy"='${updatedBy}', "updatedTime"='${updateTime}'
+	WHERE id='${id}'`,
+      (err, response) => {
+        if (err) {
+          console.log(err.stack);
+        } else {
+          console.log(response.rows);
+          res.json({ status: "successfull" });
+        }
+      }
+    );
   });
 
-  api.delete("/company/:id", (req, res) => {
-    console.log("req", req.params);
-    //take company id from path and find the id and update flag
-    res.json({ version, status: "live", method: "delete" });
-  });
+  // api.delete("/task/:id", (req, res) => {
+  //   console.log("req", req.params);
+  //   console.log("body", req.body);
+  //   console.log("body", req.body);
+  //   const {id}=req.params
+  //   const {status}=req.body;
+  //   const updatedBy="200";
+  //   const updateTime=new Date().toJSON();
+  //   db.query(`update tasks set status=${status} updatedBy=${updatedBy} updateTime=${updateTime} where id=${id}`, (err, response) => {
+  //     if (err) {
+  //       console.log(err.stack);
+  //     } else {
+  //       console.log(response.rows);
+  //       res.json({"status":"successfull","response":response.rows});
+  //     }
+  //   });
+  // });
   return api;
 };
